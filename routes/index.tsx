ca389dbf51709c7fd8e3e1da45620a415a7f244b
Nelson Hernandez <50471203+nelsoncode019@users.noreply.github.com>
2022-07-05 00:16:22 +0000

    add project

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from "preact";
+import { tw } from "@twind";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+   <div>
+    Hello with Fresh :))
+   </div>
+  );
+}
